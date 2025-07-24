245c7e24959502b0a93e54aa55abaa15d40e71db
Shane-Scarcella <shanescarcella@gmail.com>
2025-07-24 16:35:08 -0400

    feat: Initial project scaffolding

---
+++ api/api/src/test/java/com/shanescarcella/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shanescarcella.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
