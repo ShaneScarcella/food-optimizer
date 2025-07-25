e508df0b441ecfb1137ef1d1885ade172257e3c4
Shane-Scarcella <shanescarcella@gmail.com>
2025-07-24 20:26:37 -0400

    feat(api): Added password encoding in UserService

--- api/api/src/main/java/com/shanescarcella/api/user/UserService.java
+++ api/api/src/main/java/com/shanescarcella/api/user/UserService.java
@@ -1,6 +1,7 @@
 package com.shanescarcella.api.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,5 +9,14 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    public User createUser(User user) {
+        // TODO: Add check to see if user email already exists
+
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        return userRepository.save(user);
+    }
 }
