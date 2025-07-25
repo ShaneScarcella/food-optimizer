44208df8eb9d3fea81c5d0521402a31c692a1b4d
Shane-Scarcella <shanescarcella@gmail.com>
2025-07-24 21:32:01 -0400

    feat(api): Created AuthController and register endpoint

---
+++ api/api/src/main/java/com/shanescarcella/api/auth/AuthController.java
@@ -0,0 +1,25 @@
+package com.shanescarcella.api.auth;
+
+import com.shanescarcella.api.user.User;
+import com.shanescarcella.api.user.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@Valid @RequestBody User user) {
+        userService.createUser(user);
+        return ResponseEntity.ok("User registered successfully!");
+    }
+}
