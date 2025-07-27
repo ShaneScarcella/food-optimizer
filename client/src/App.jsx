e7c8fbc6085a7599181a478fe20e355f3b22caf1
Shane-Scarcella <shanescarcella@gmail.com>
2025-07-27 17:31:06 -0400

    feat(client): Set up initial pages and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import DashboardPage from './pages/DashboardPage';
+
 function App() {
   return (
-    <h1>Food Optimizer App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/" element={<DashboardPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
