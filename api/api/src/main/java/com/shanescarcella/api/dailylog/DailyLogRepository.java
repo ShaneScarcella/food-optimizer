9bdcfa81ec42b25929d76348ae772c1527721146
Shane-Scarcella <shanescarcella@gmail.com>
2025-07-31 00:55:12 -0400

    feat(api): Created DailyLogRepository

---
+++ api/api/src/main/java/com/shanescarcella/api/dailylog/DailyLogRepository.java
@@ -0,0 +1,9 @@
+package com.shanescarcella.api.dailylog;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.time.LocalDate;
+import java.util.Optional;
+
+public interface DailyLogRepository extends MongoRepository<DailyLog, String> {
+    Optional<DailyLog> findByUserIdAndDate(String userId, LocalDate date);
+}
