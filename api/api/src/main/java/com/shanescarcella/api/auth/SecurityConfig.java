27c995d979207312bc1a12a42aecac3f6018aab7
Shane-Scarcella <shanescarcella@gmail.com>
2025-09-16 16:49:16 -0400

    fix(security): Permits CORS pre-flight OPTIONS requests

--- api/api/src/main/java/com/shanescarcella/api/auth/SecurityConfig.java
+++ api/api/src/main/java/com/shanescarcella/api/auth/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.shanescarcella.api.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -38,6 +39,7 @@ public class SecurityConfig {
             .cors(Customizer.withDefaults())
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() 
                 .requestMatchers("/api/auth/**").permitAll()
                 .anyRequest().authenticated()
             )
